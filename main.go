552b9204f43bab412bea58e6ce11c04c64ce7d26
makotia <3mdevmail@gmail.com>
2019-08-08 12:29:24 +0900

    :+1: add cli args

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"os"
 
+	"github.com/makotia/exifer/modules"
 	"github.com/urfave/cli"
 )
 
@@ -12,6 +13,20 @@ func main() {
 	app.Name = "Exifer"
 	app.Usage = "📸 A small CLI tool when use tweets your boast photos."
 	app.Version = "0.0.1"
+
+	app.Action = func(c *cli.Context) error {
+		if c.Bool("image") {
+			modules.Decode(c.Args().Get(0))
+		}
+		return nil
+	}
+
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{
+			Name:  "image, i",
+			Usage: "Add watermark to image.",
+		},
+	}
 
 	app.Run(os.Args)
 }
